43a4d3e41d1fb827d35b7433a95607436ff89737
FranklinMiranda <fdm61344@gmail.com>
2023-03-06 08:55:27 -0500

    CM

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/todos', (req, res, next) => {
+    // get placeholder
+});
+
+router.post('/todos', (req, res, next) => {
+    //post placeholder
+    
+});
+
+router.delete('/todos', (req, res, next) => {
+    // delete placeholder
+});
